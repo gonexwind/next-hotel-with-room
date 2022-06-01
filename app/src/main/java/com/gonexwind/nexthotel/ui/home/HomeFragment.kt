9d7285bdba7a1bffa47de741b10cc00ae26584e2
gonexwind <fikky.ardi15@gmail.com>
2022-06-01 10:32:23 +0700

    home ui

--- app/src/main/java/com/gonexwind/nexthotel/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gonexwind/nexthotel/ui/home/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.gonexwind.nexthotel.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,17 +20,8 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
