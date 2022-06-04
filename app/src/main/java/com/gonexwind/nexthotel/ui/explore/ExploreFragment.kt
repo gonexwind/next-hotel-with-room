7102d33c30fcdeee1c22f602242113f073fb251a
gonexwind <fikky.ardi15@gmail.com>
2022-06-04 09:11:39 +0700

    implement explore

--- app/src/main/java/com/gonexwind/nexthotel/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/gonexwind/nexthotel/ui/explore/ExploreFragment.kt
@@ -1,60 +1,76 @@
 package com.gonexwind.nexthotel.ui.explore
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.gonexwind.nexthotel.R
+import androidx.fragment.app.Fragment
+import com.gonexwind.nexthotel.adapter.HotelVerticalAdapter
+import com.gonexwind.nexthotel.api.ApiConfig
+import com.gonexwind.nexthotel.databinding.FragmentExploreBinding
+import com.gonexwind.nexthotel.model.HotelsResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ExploreFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ExploreFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentExploreBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+    ): View {
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getHotels()
+    }
+
+    private fun getHotels() {
+        showLoading(true)
+        val client = ApiConfig.getApiService().getListHotel()
+        client.enqueue(object : Callback<HotelsResponse> {
+            override fun onResponse(
+                call: Call<HotelsResponse>,
+                response: Response<HotelsResponse>
+            ) {
+                showLoading(false)
+                val responseBody = response.body()
+                if (responseBody != null) {
+                    val verticalAdapter = HotelVerticalAdapter(responseBody.data)
+                    binding.verticalRecyclerView.adapter = verticalAdapter
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<HotelsResponse>, t: Throwable) {
+                showLoading(false)
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+
+    fun showLoading(isLoading: Boolean) {
+        if (isLoading)
+            binding.progressBar.visibility = View.VISIBLE
+        else
+            binding.progressBar.visibility = View.GONE
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ExploreFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ExploreFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val TAG = "ExploreFragment"
     }
 }
