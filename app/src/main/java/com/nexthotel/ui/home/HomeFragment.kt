c73a4981e204daace2f00c4a27754e49d04acbc7
gonexwind <fikky.ardi15@gmail.com>
2022-06-16 07:12:30 +0700

    refactor code

--- app/src/main/java/com/nexthotel/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nexthotel/ui/home/HomeFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.nexthotel.R
 import com.nexthotel.core.data.Result
+import com.nexthotel.core.ui.BestPickAdapter
+import com.nexthotel.core.ui.HotelForYouAdapter
 import com.nexthotel.core.ui.ViewModelFactory
+import com.nexthotel.core.utils.Utils.toast
 import com.nexthotel.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -40,38 +42,20 @@ class HomeFragment : Fragment() {
         val bestPickAdapter = BestPickAdapter {
             if (it.isBookmarked) {
                 viewModel.deleteHotel(it)
-                Toast.makeText(
-                    requireActivity(),
-                    getString(R.string.unbookmarked_toast),
-                    Toast.LENGTH_SHORT
-                )
-                    .show()
+                toast(requireActivity(), getString(R.string.unbookmarked_toast))
             } else {
                 viewModel.saveHotel(it)
-                Toast.makeText(
-                    requireActivity(),
-                    getString(R.string.bookmark_toast),
-                    Toast.LENGTH_SHORT
-                ).show()
+                toast(requireActivity(), getString(R.string.bookmark_toast))
             }
         }
 
         val hotelForYouAdapter = HotelForYouAdapter {
             if (it.isBookmarked) {
                 viewModel.deleteHotel(it)
-                Toast.makeText(
-                    requireActivity(),
-                    getString(R.string.unbookmarked_toast),
-                    Toast.LENGTH_SHORT
-                )
-                    .show()
+                toast(requireActivity(), getString(R.string.unbookmarked_toast))
             } else {
                 viewModel.saveHotel(it)
-                Toast.makeText(
-                    requireActivity(),
-                    getString(R.string.bookmark_toast),
-                    Toast.LENGTH_SHORT
-                ).show()
+                toast(requireActivity(), getString(R.string.bookmark_toast))
             }
         }
 
@@ -86,11 +70,7 @@ class HomeFragment : Fragment() {
                     }
                     is Result.Error -> {
                         showLoading(true)
-                        Toast.makeText(
-                            context,
-                            "Please Check Your Internet",
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        toast(requireActivity(), getString(R.string.check_internet))
                     }
                 }
             }
@@ -107,11 +87,7 @@ class HomeFragment : Fragment() {
                     }
                     is Result.Error -> {
                         showLoading(true)
-                        Toast.makeText(
-                            context,
-                            "Please Check Your Internet",
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        toast(requireActivity(), getString(R.string.check_internet))
                     }
                 }
             }
